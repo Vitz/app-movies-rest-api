11c8702ea083df25f91b1c896c2c9d7f7e26e8d8
Zal.ewski <patrykorganisciak@gmail.com>
2019-07-05 18:53:18 +0200

    Heroku deploy omg omg omg

--- movies/movies/wsgi.py
+++ movies/movies/wsgi.py
@@ -5,7 +5,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "movies.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "movies.movies.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
