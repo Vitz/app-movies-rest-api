57a69b620e754b5312f027f773dee340dd1f29da
Vitz <viton007@op.pl>
2019-10-29 11:41:54 +0100

    Remove '*' its incorrect

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -145,10 +145,10 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
-    '*',
+    
 ]
 CORS_ORIGIN_REGEX_WHITELIST = [
     'http://localhost:3030',
-    '*',
+  
 
 ]
